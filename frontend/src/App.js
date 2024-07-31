2c4b0b830e757c9df4b999b019b7d8431b923f56
lia-weng <lia.xin.weng@gmail.com>
2024-07-31 15:13:51 -0400

    added frontend template

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import "aos/dist/aos.css";
+import './index.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
+// All pages
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import DemoProduct from './pages/DemoProduct';
+
+import {useDocTitle} from './components/CustomHook';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
+  useEffect(() => {
+    const aos_init = () => {
+      AOS.init({
+        once: true,
+        duration: 1000,
+        easing: 'ease-out-cubic',
+      });
+    }
+
+    window.addEventListener('load', () => {
+      aos_init();
+    });
+  }, []);
+
+  useDocTitle("MLD | Molad e Konsult - Bespoke Web and Mobile Applications");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <ScrollToTop>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/get-demo" element={<DemoProduct />} /> 
+          </Routes>
+        </ScrollToTop>
+      </Router>
+    </>
   );
 }
+
 
 export default App;
